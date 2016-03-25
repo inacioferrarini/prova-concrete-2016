152a8fc280e19e7e573b911b669d832f20bd027e
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-25 16:45:36 -0300

    Initial Project Version. .gitignore file added. The project was created from Xcode template including tests and Core Data support. Cocoa Pods support added

---
+++ GitHubSpy/GitHubSpy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubSpy
+//
+//  Created by José Inácio Athayde Ferrarini on 25/03/16.
+//  Copyright © 2016 com.inacioferrarini.concrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
