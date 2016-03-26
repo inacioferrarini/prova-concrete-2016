2afa1939ce0c3e15b3adb07b04bb1042d174d919
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-25 23:33:40 -0300

    Adding initial version of view controllers for repositories and pull requests. Adding view controllers to the manager, including appContext injection. Updated Storyboard with base UI design for repositories and pull requests.

--- GitHubSpy/GitHubSpy/Foundation/ViewController/ViewControllerManager.swift
+++ GitHubSpy/GitHubSpy/Foundation/ViewController/ViewControllerManager.swift
@@ -1,5 +1,19 @@
 import UIKit
 
 class ViewControllerManager: NSObject {
-        
+    
+    func repositoryListTableViewController(appContext: AppContext!) -> RepositoryListTableViewController {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let repositoryListTableViewController = storyBoard.instantiateViewControllerWithIdentifier("RepositoryListTableViewController") as! RepositoryListTableViewController
+        repositoryListTableViewController.appContext = appContext
+        return repositoryListTableViewController
+    }
+    
+    func pullRequestListTableViewController(appContext: AppContext!) -> PullRequestListTableViewController {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let pullRequestListTableViewController = storyBoard.instantiateViewControllerWithIdentifier("PullRequestListTableViewController") as! PullRequestListTableViewController
+        pullRequestListTableViewController.appContext = appContext
+        return pullRequestListTableViewController
+    }
+    
 }
