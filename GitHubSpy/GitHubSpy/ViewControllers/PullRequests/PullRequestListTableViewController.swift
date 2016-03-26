74f55008611ea5671181ac01408f7fb7720ab5bd
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-26 00:33:15 -0300

    Updating Internationalization structure for the project.

--- GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class PullRequestListTableViewController: BaseTableViewController {
 
+    // MARK: - BaseTableViewController override
+    
+    override func viewControllerTitle() -> String? {
+        return NSLocalizedString("VC_PULL_REQUEST_LIST_TITLE", comment: "VC_PULL_REQUEST_LIST_TITLE")
+    }
+    
 }
--- GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class RepositoryListTableViewController: BaseTableViewController {
 
+    // MARK: - BaseTableViewController override
+    
+    override func viewControllerTitle() -> String? {
+        return NSLocalizedString("VC_REPOSITORY_LIST_TITLE", comment: "VC_REPOSITORY_LIST_TITLE")
+    }
+    
 }
