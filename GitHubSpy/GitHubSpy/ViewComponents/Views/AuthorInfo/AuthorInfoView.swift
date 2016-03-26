56a504d30f7e2df0c184ba37d19f0eca93588a2b
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-26 17:44:45 -0300

    Initial version of the view component.

--- GitHubSpy/GitHubSpy/ViewComponents/Views/AuthorInfo/AuthorInfoView.swift
+++ GitHubSpy/GitHubSpy/ViewComponents/Views/AuthorInfo/AuthorInfoView.swift
@@ -1,19 +1,5 @@
 import UIKit
 
-class AuthorInfoView: UIView {
-
-//
-//    passar para uma base class
-//
-//    override func awakeAfterUsingCoder(aDecoder: NSCoder) -> AnyObject? {
-//        if self.subviews.count == 0 {
-//            return loadNib()
-//        }
-//        return self
-//    }
-//    
-//    private func loadNib() -> AuthorInfoView {
-//        return NSBundle.mainBundle().loadNibNamed("AuthorInfoView", owner: nil, options: nil)[0] as! AuthorInfoView
-//    }
-    
+class AuthorInfoView: ViewComponent {
+   
 }
