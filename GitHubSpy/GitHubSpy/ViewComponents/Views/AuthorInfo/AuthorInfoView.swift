9686a6fa9b8f2e343ac6ccd46a0e44f3c83035f9
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-26 16:29:38 -0300

    adding initial version for view component

---
+++ GitHubSpy/GitHubSpy/ViewComponents/Views/AuthorInfo/AuthorInfoView.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class AuthorInfoView: UIView {
+
+//
+//    passar para uma base class
+//
+//    override func awakeAfterUsingCoder(aDecoder: NSCoder) -> AnyObject? {
+//        if self.subviews.count == 0 {
+//            return loadNib()
+//        }
+//        return self
+//    }
+//    
+//    private func loadNib() -> AuthorInfoView {
+//        return NSBundle.mainBundle().loadNibNamed("AuthorInfoView", owner: nil, options: nil)[0] as! AuthorInfoView
+//    }
+    
+}
