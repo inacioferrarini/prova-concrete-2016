3b71d00adb707909ebacd73e46a3beb85d4164a8
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-26 16:31:22 -0300

    Adding datasources to view controllers.

--- GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
@@ -8,4 +8,35 @@ class PullRequestListTableViewController: BaseTableViewController {
         return NSLocalizedString("VC_PULL_REQUEST_LIST_TITLE", comment: "VC_PULL_REQUEST_LIST_TITLE")
     }
     
+    override func shouldCheckTodayData() -> Bool {
+        return false
+    }
+    
+    override func syncDataWithServer() {
+        self.syncDataComplete()
+    }
+    
+    override func createDataSource() -> UITableViewDataSource? {
+        
+        let presenter = TableViewCellPresenter<UITableViewCell, EntityPullRequest>(
+            configureCellBlock: { (cell: UITableViewCell, entity:EntityPullRequest) -> Void in
+                //            PullRequestTableViewCell
+                print(" Table view cell presenter configure cell block ... \(entity)")
+            }, cellReuseIdentifier: "PullRequestTableViewCell")
+        
+        let sortDescriptors:[NSSortDescriptor] = [] // TODO: update
+        let context = self.appContext.coreDataStack.managedObjectContext
+        let logger = appContext.logger
+        
+        let dataSource = FetcherDataSource<EntityPullRequest>(
+            targetingTableView: self.tableView!,
+            presenter: presenter,
+            entityName: EntityRepository.entityName(),
+            sortDescriptors: sortDescriptors,
+            managedObjectContext: context,
+            logger: logger)
+        
+        return dataSource
+    }
+
 }
--- GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
@@ -8,4 +8,35 @@ class RepositoryListTableViewController: BaseTableViewController {
         return NSLocalizedString("VC_REPOSITORY_LIST_TITLE", comment: "VC_REPOSITORY_LIST_TITLE")
     }
     
+    override func shouldCheckTodayData() -> Bool {
+        return false
+    }
+    
+    override func syncDataWithServer() {
+        self.syncDataComplete()
+    }
+    
+    override func createDataSource() -> UITableViewDataSource? {
+        
+        let presenter = TableViewCellPresenter<UITableViewCell, EntityRepository>(
+            configureCellBlock: { (cell: UITableViewCell, entity:EntityRepository) -> Void in
+    //            RepositoryTableViewCell
+                print(" Table view cell presenter configure cell block ... \(entity)")
+            }, cellReuseIdentifier: "RepositoryTableViewCell")
+        
+        let sortDescriptors:[NSSortDescriptor] = [] // TODO: update
+        let context = self.appContext.coreDataStack.managedObjectContext
+        let logger = appContext.logger
+        
+        let dataSource = FetcherDataSource<EntityRepository>(
+            targetingTableView: self.tableView!,
+            presenter: presenter,
+            entityName: EntityRepository.entityName(),
+            sortDescriptors: sortDescriptors,
+            managedObjectContext: context,
+            logger: logger)
+        
+        return dataSource
+    }
+    
 }
