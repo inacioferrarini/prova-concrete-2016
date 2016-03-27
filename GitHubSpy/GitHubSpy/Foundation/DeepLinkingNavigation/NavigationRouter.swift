3cef60e8bc9604631b5298a813a3fe16059a3ac1
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-27 17:48:07 -0300

    Passing the routes as parameter in order to make it more testable.

--- GitHubSpy/GitHubSpy/Foundation/DeepLinkingNavigation/NavigationRouter.swift
+++ GitHubSpy/GitHubSpy/Foundation/DeepLinkingNavigation/NavigationRouter.swift
@@ -4,44 +4,19 @@ import UIKit
 class NavigationRouter: NSObject {
     
     let schema:String!
-    let coreDataStack: CoreDataStack!
-    let baseNavigationController: UINavigationController!
     let logger:Logger!
     
-    init(schema:String!, coreDataStack: CoreDataStack!, baseNavigationController: UINavigationController!, logger:Logger!) {
+    init(schema:String!, logger:Logger!) {
         self.schema = schema
-        self.coreDataStack = coreDataStack
-        self.baseNavigationController = baseNavigationController
         self.logger = logger
         super.init()
     }
     
-    func registerRoutes() {
+    func registerRoutes(appRoutes: [RoutingElement]) {
         let routes = JLRoutes(forScheme: self.schema)
         
-        routes.addRoute("/") { (params: [NSObject : AnyObject]) -> Bool in
-            
-            self.logger.logInfo("Showing root ... ")
-            
-            return true
-        }
-        
-        routes.addRoute("/pr/:owner/:repo") { (params: [NSObject : AnyObject]) -> Bool in
-            guard let owner = params["owner"] as? String else { return false }
-            guard let repository = params["repo"] as? String else { return false }
-            
-            let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
-            let appContext = delegate.appContext!
-            let context = self.coreDataStack.managedObjectContext
-            
-            let viewController = ViewControllerManager().pullRequestListTableViewController(appContext)
-            if let owner = EntityAuthor.entityAuthorWithLogin(owner, inManagedObjectContext: context),
-                let repository = EntityRepository.entityRepositoryWithName(repository, owner: owner, inManagedObjectContext: context) {
-                viewController.repository = repository
-            }
-            self.baseNavigationController.pushViewController(viewController, animated: true)
-            
-            return true
+        for r:RoutingElement in appRoutes {
+            routes.addRoute(r.pattern, handler: r.handler)
         }
     }
     
