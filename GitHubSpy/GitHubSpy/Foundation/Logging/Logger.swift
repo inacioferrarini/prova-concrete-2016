951f432ad83c030fea155b76632060bb21ea0eff
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-27 20:43:14 -0300

    Asking string message error logger.

--- GitHubSpy/GitHubSpy/Foundation/Logging/Logger.swift
+++ GitHubSpy/GitHubSpy/Foundation/Logging/Logger.swift
@@ -5,6 +5,10 @@ class Logger: NSObject {
     func logError(error:NSError) {
         print("Unresolved error \(error), \(error.userInfo)")
     }
+
+    func logErrorMessage(errorMessage:String) {
+        print("Error: \(errorMessage)")
+    }
     
     func logInfo(message:String) {
         print("Info: \(message)")
