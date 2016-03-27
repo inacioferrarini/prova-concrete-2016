fa52862f7e6df05c99dda5b16486dc6bc8b318fa
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-26 23:01:14 -0300

    Updating view components

--- GitHubSpy/GitHubSpy/ViewComponents/Views/AuthorInfo/AuthorInfoView.swift
+++ GitHubSpy/GitHubSpy/ViewComponents/Views/AuthorInfo/AuthorInfoView.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class AuthorInfoView: ViewComponent {
    
+    @IBOutlet weak var userAvatarImage: UIImageView!
+    @IBOutlet weak var userLoginLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
 }
