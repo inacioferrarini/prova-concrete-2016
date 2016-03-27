faf2fcfbce5b501622ae1cb18256c6abaa096bd3
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-27 16:54:19 -0300

    Creating outlets from storyboard to the PullRequestTableViewCell.

--- GitHubSpy/GitHubSpy/ViewComponents/TableViewCells/PullRequests/PullRequestTableViewCell.swift
+++ GitHubSpy/GitHubSpy/ViewComponents/TableViewCells/PullRequests/PullRequestTableViewCell.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class PullRequestTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var pullRequestTitleLabel: UILabel!
+    @IBOutlet weak var pullRequestBodyLabel: UILabel!
+    @IBOutlet weak var authorInfoView: AuthorInfoView!
+    
 }
