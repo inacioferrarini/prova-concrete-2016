acdcc363c97b68f1daa73b75bb5783155c96253b
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-28 04:28:30 -0300

    Updating properties.

--- GitHubSpy/GitHubSpy/Persistence/Entities/EntityPullRequest+CoreDataProperties.swift
+++ GitHubSpy/GitHubSpy/Persistence/Entities/EntityPullRequest+CoreDataProperties.swift
@@ -3,12 +3,13 @@ import CoreData
 
 extension EntityPullRequest {
 
-    @NSManaged var title: String?
     @NSManaged var body: String?
     @NSManaged var created: NSDate?
     @NSManaged var lastUpdated: NSDate?
-    @NSManaged var url: String?
+    @NSManaged var title: String?
     @NSManaged var uid: NSNumber?
+    @NSManaged var url: String?
+    @NSManaged var status: String?
     @NSManaged var owner: EntityAuthor?
     @NSManaged var repository: EntityRepository?
 
