2ee011fc9ec3a2c8a50d5b0684168b523b8dab3d
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-28 01:50:11 -0300

    Moved methods related to son processing to specific extensions in order to make the ViewControllers smaller.

--- GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/PullRequests/PullRequestListTableViewController.swift
@@ -36,34 +36,7 @@ class PullRequestListTableViewController: BaseTableViewController {
         }
         
     }
-    
-    func processPullRequests(pullRequests: [PullRequest]) {
-        for pullRequest in pullRequests {
-            self.processPullRequest(pullRequest)
-        }
-    }
-    
-    func processPullRequest(pullRequest: PullRequest) {
-        let ctx = self.appContext.coreDataStack.managedObjectContext
-        
-        if let repository = self.repository,
-            let entityAuthor = repository.owner,
-            let pullRequestUid = pullRequest.uid,
-            let entityPullRequest = EntityPullRequest.entityPullRequestWithUid(pullRequestUid, owner: entityAuthor, repository: repository, inManagedObjectContext: ctx) {
-
-                entityAuthor.login = pullRequest.ownerLogin
-                entityAuthor.firstName = ""
-                entityAuthor.lastName = ""
-                entityAuthor.avatarUrl = pullRequest.ownerAvatarUrl
-                
-                entityPullRequest.title = pullRequest.title ?? ""
-                entityPullRequest.body = pullRequest.body ?? ""
-                entityPullRequest.created = pullRequest.createdDate
-                entityPullRequest.lastUpdated = pullRequest.updatedDate
-                entityPullRequest.url = pullRequest.url ?? ""
-        }
-    }
-    
+       
     override func createDataSource() -> UITableViewDataSource? {
         
         let presenter = TableViewCellPresenter<UITableViewCell, EntityPullRequest>(
--- GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
+++ GitHubSpy/GitHubSpy/ViewControllers/Repositories/RepositoryListTableViewController.swift
@@ -27,31 +27,6 @@ class RepositoryListTableViewController: BaseTableViewController {
         }
     }
     
-    func processRepositories(repositories: [Repository]) {
-        for repository in repositories {
-            self.processRepository(repository)
-        }
-    }
-    
-    func processRepository(repository: Repository) {
-        let ctx = self.appContext.coreDataStack.managedObjectContext
-        
-        if let authorLogin = repository.ownerLogin,
-            let entityAuthor = EntityAuthor.entityAuthorWithLogin(authorLogin, inManagedObjectContext: ctx),
-            let entityRepositoryName = repository.name,
-            let entityRepository = EntityRepository.entityRepositoryWithName(entityRepositoryName, owner: entityAuthor, inManagedObjectContext: ctx) {
-                
-                entityAuthor.login = repository.ownerLogin
-                entityAuthor.firstName = ""
-                entityAuthor.lastName = ""
-                entityAuthor.avatarUrl = repository.ownerAvatarUrl
-                
-                entityRepository.descriptionText = repository.descriptionText ?? ""
-                entityRepository.forksCount = repository.forksCount ?? 0
-                entityRepository.starsCount = repository.starsCount ?? 0
-        }
-    }
-    
     override func createDataSource() -> UITableViewDataSource? {
         
         let presenter = TableViewCellPresenter<UITableViewCell, EntityRepository>(
