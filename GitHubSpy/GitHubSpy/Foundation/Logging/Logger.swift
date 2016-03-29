4ef50f6abcfd1213f899ac4d12e74a32c9006335
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-03-29 20:52:23 -0300

    Adding CocoaLumberjack to handle logging.

--- GitHubSpy/GitHubSpy/Foundation/Logging/Logger.swift
+++ GitHubSpy/GitHubSpy/Foundation/Logging/Logger.swift
@@ -1,17 +1,31 @@
 import UIKit
+import CocoaLumberjack
 
 class Logger: NSObject {
 
+    override init() {
+        DDLog.addLogger(DDASLLogger.sharedInstance())
+        DDLog.addLogger(DDTTYLogger.sharedInstance())
+        
+        let fileLogger: DDFileLogger = DDFileLogger()
+        fileLogger.maximumFileSize = 1024 * 1024
+        fileLogger.rollingFrequency = 60 * 60 * 24
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.addLogger(fileLogger)
+        super.init()
+    }
+    
     func logError(error:NSError) {
-        print("Unresolved error \(error), \(error.userInfo)")
+        DDLogError("\(error), \(error.userInfo)")
     }
 
+    // TODO: Disable this method.
     func logErrorMessage(errorMessage:String) {
-        print("Error: \(errorMessage)")
+        DDLogError(errorMessage)
     }
     
     func logInfo(message:String) {
-        print("Info: \(message)")
+        DDLogInfo(message)
     }
     
 }
