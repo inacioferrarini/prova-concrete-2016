196a7995abc30675fb34d9c5d02a5d5bbc9dfe29
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-02 11:17:45 -0300

    Adding unit tests.

---
+++ GitHubSpy/GitHubSpyTests/Foundation/Persistence/CoreDataStackTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+class CoreDataStackTests: XCTestCase {
+    
+    func test_saveContext_mustThrowException() {
+        let coreDataStack = TestUtil().createCoreDataStack()
+        let ctx = coreDataStack.managedObjectContext
+        let ruleName = TestUtil().randomRuleName()
+        if let testEntity = EntitySyncHistory.entityAutoSyncHistoryByName(ruleName, lastExecutionDate: nil, inManagedObjectContext: ctx) {
+            testEntity.ruleName = nil
+        }
+        coreDataStack.saveContext()
+    }
+    
+}
