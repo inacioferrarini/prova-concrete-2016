b11622c710d96537643eb7e1b64c3b71a226965f
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-02 15:35:42 -0300

    Updating tests. JLRoutes mock no longer needed, thus removed. Fixed urls issues. It was using fileURLWithPath:, not string: . Fixed. Working as expected.

--- GitHubSpy/GitHubSpyTests/Foundation/DeepLinkingNavigation/NavigationRouterTests.swift
+++ GitHubSpy/GitHubSpyTests/Foundation/DeepLinkingNavigation/NavigationRouterTests.swift
@@ -5,25 +5,31 @@ class NavigationRouterTests: XCTestCase {
     
     override func setUp() {
         self.router = self.createNavigationRouter()
+        self.routeWasCalled = false
         super.setUp()
     }
     
+    var routeWasCalled:Bool = false
     var router:NavigationRouter!
     
     func createNavigationRouter() -> NavigationRouter {
+        JLRoutes.setVerboseLoggingEnabled(true)
         let navigationRouter = NavigationRouter(schema: "TesteScheme", logger: Logger())
+        let route = RoutingElement(pattern: "/testUrlScheme") { (params: [NSObject : AnyObject]) -> Bool in
+            self.routeWasCalled = true
+            return true
+        }
+        navigationRouter.registerRoutes([route])
         return navigationRouter
     }
     
     func test_dispatch_mustReturnTrue() {
-        let navigationRouter = NavigationRouter(router: JLRoutesMock(), schema:
-                    "TesteScheme", logger: Logger())
-        let result = navigationRouter.dispatch(NSURL(fileURLWithPath: "TesteScheme://invalidTestUrlScheme"))
+        let result = self.router.dispatch(NSURL(string: "TesteScheme://testUrlScheme")!)
         XCTAssertTrue(result)
     }
     
     func test_dispatch_mustReturnFalse() {
-        let result = self.router.dispatch(NSURL(fileURLWithPath: "TesteScheme://invalidTestUrlScheme"))
+        let result = self.router.dispatch(NSURL(string: "TesteScheme://invalidTestUrlScheme")!)
         XCTAssertFalse(result)
     }
     
