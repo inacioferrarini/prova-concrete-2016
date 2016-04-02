34c9e23775ae9ac56bc04d2a508dae13abc3455b
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-02 11:22:26 -0300

    Removing unused methods.

--- GitHubSpy/GitHubSpyTests/Foundation/TestUtil.swift
+++ GitHubSpy/GitHubSpyTests/Foundation/TestUtil.swift
@@ -8,19 +8,7 @@ class TestUtil: XCTestCase {
     func createCoreDataStack() -> CoreDataStack {
         return CoreDataStack(modelFileName: TestUtil.modelFileName, databaseFileName: TestUtil.databaseFileName, logger: Logger())
     }
-    
-    func purgeDB(dbName:String) {
-        if let applicationDocumentsDirectory = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last {
-            
-            do {
-                let url = applicationDocumentsDirectory.URLByAppendingPathComponent("\(dbName).sqlite")
-                try! NSFileManager.defaultManager().removeItemAtURL(url)
-            } catch {
-                print(error)
-            }
-        }
-    }
-    
+        
     func randomRuleName() -> String {
         return NSUUID().UUIDString.stringByReplacingOccurrencesOfString("-", withString: "")
     }
