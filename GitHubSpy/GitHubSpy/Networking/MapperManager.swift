c8892bbc44597f7cf14694fe5269b353a2159166
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-13 14:23:03 -0300

    Updating assets.

---
+++ GitHubSpy/GitHubSpy/Networking/MapperManager.swift
@@ -0,0 +1,13 @@
+//
+//  MapperManager.swift
+//  GitHubSpy
+//
+//  Created by José Inácio Athayde Ferrarini on 13/04/16.
+//  Copyright © 2016 com.inacioferrarini.concrete. All rights reserved.
+//
+
+import UIKit
+
+class MapperManager: NSObject {
+
+}
